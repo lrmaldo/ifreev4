2d416cb536f5223d2ea3bbe16749961ce897b44b
Leonardo Maldonado <lrmaldo@gmail.com>
2025-11-12 17:23:44 -0600

    feat: Actualizar metadatos SEO y enlaces de icono en la cabecera

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,31 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ asset('img/Logo i-Free.png') }}" sizes="any">
+<link rel="icon" href="{{ asset('img/Logo i-Free.png') }}" type="image/svg+xml">
+<link rel="apple-touch-icon" href="{{ asset('img/Logo i-Free.png') }}">
+
+{{-- autor --}}
+<meta name="author" content="i-Free - Portal Cautivo WiFi" />
+{{-- descripción --}}
+<meta name="description" content="{{ $description ?? 'i-Free - Portal Cautivo WiFi' }}" />
+{{-- palabras clave --}}
+<meta name="keywords" content="{{ $keywords ?? 'i-Free, Portal Cautivo, WiFi, Hotspot, Acceso a Internet' }}" />
+{{-- robots --}}
+<meta name="robots" content="index, follow" />
+{{-- tema color --}}
+<meta name="theme-color" content="#2563EB" />
+
+{{-- SEO Meta Tags --}}
+<meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+<meta property="og:description" content="{{ $description ?? 'i-Free ' }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:image" content="{{ asset('img/Logo i-Free.png') }}" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="{{ $title ?? config('app.name') }}" />
+<meta name="twitter:description" content="{{ $description ?? 'i-Free - Portal Cautivo WiFi' }}" />
+<meta name="twitter:image" content="{{ asset('img/Logo i-Free.png') }}" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
